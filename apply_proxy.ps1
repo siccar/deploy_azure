21871a6e6c10e6ba2b5d15189e229d2ef121747c
Stuart Fraser <stuart@wallet.services>
2022-07-07 09:30:43 +0100

    proxy updates

--- apply_proxy.ps1
+++ apply_proxy.ps1
@@ -1,13 +1,14 @@
 # Setup ingress-nginx
-[CmdletBinding()]
-param (
-    [Parameter(Mandatory=$true)] [string] $InstallationDNSName='node0.siccar.dev' 
-    )
+#[CmdletBinding()]
+#param (
+#    [Parameter(Mandatory=$false)] [string] $InstallationDNSName='node0.siccar.dev' 
+#    )
 
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.2.1/deploy/static/provider/cloud/deploy.yaml
 
 
-
 (Get-Content ./sourceyaml/siccar-ingress.yaml) | `
-    ForEach-Object { $_.replace("{{INSTALLATIONDNS}}", "$InstallationDNSName")   } | `
-    Out-File ./components/siccar-ingress.yaml
+    ForEach-Object { $_.replace("{{INSTALLATIONDNS}}", "$env:InstallationDNSName")   } | `
+    Out-File ./deployments/siccar-ingress.yaml
+
+kubectl apply -f ./deployments/siccar-ingress.yaml
