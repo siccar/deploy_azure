576c72e1ffbf0df787274130f9d268f9a4dd80ee
Stuart Fraser <stuart@wallet.services>
2023-07-27 12:01:52 +0100

    update for instrumentation

--- apply_instrumentation.ps1
+++ apply_instrumentation.ps1
@@ -4,6 +4,10 @@ param (
     [Parameter(Mandatory=$true, HelpMessage="Enter your Application Insights Intrumentation Key")] [string] $instrumentation_key=""
 )
 
+# do appinsights first
+kubectl create secret generic appinsights-instrumentationkey --from-literal=key=$instrumentation_key
+
+# then lets use open telementry
 $otel_yaml = (Get-Content -path ./sourceyaml/open-telemetry-collector-app-insights.yaml -Raw) -replace '<INSTRUMENTATION-KEY>', $instrumentation_key
 
 Set-Content -Path ./sourceyaml/open-telemetry-collector-app-insights_temp.yaml -Value $otel_yaml 
