a35d952d68fa91fd002d9a2a6af86cc4ef8f2761
Stuart Fraser <stuart@stuartfraser.net>
2023-08-01 10:38:16 +0100

    reorder restart

--- restart_services.ps1
+++ restart_services.ps1
@@ -1,9 +1,9 @@
+kubectl rollout restart deployment/register-service
 kubectl rollout restart deployment/adminui
 kubectl rollout restart deployment/action-service
 kubectl rollout restart deployment/blueprint-service
 kubectl rollout restart deployment/peer-service
 kubectl rollout restart deployment/tenant-service
-kubectl rollout restart deployment/register-service
 kubectl rollout restart deployment/validator-service
 kubectl rollout restart deployment/wallet-service
 kubectl get pods
